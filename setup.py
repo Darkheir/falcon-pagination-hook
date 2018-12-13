dec22c36056bd199b89fbc923190589347b32ddb
Raphaël Cohen <raphael.cohen.utt@gmail.com>
2018-12-13 23:58:17 +0100

    Adapts code to python3.6

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ classifiers = [
     'Intended Audience :: Developers',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Software Development :: Libraries',
 ]
 
